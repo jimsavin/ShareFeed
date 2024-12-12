98f93029480e5ddea2377c326967fe406780a303
Dmytro Savin <>
2024-12-12 12:41:56 +1100

    'RemoteFeedLoader' does not load data upon creation.

---
+++ ShareFeed/ShareFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,22 @@
+
+import Testing
+import XCTest
+@testable import ShareFeed
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+}

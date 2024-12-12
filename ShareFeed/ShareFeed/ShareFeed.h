f969065b24363f3d5c73915da7bc93f1ce42065e
Dmytro Savin <>
2024-12-12 11:59:50 +1100

    Project setup.

---
+++ ShareFeed/ShareFeed/ShareFeed.h
@@ -0,0 +1,18 @@
+//
+//  ShareFeed.h
+//  ShareFeed
+//
+//  Created by Dmytro Savin on 12.12.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ShareFeed.
+FOUNDATION_EXPORT double ShareFeedVersionNumber;
+
+//! Project version string for ShareFeed.
+FOUNDATION_EXPORT const unsigned char ShareFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShareFeed/PublicHeader.h>
+
+
---
+++ ShareFeed/ShareFeedTests/ShareFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShareFeedTests.swift
+//  ShareFeedTests
+//
+//  Created by Dmytro Savin on 12.12.2024.
+//
+
+import Testing
+@testable import ShareFeed
+
+struct ShareFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
